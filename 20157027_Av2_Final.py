fddbc2a2054cfaaaeb6cd00b10c75e2a655a4e86
renanrrj <david.ramos@edb.br>
2022-11-21 20:51:56 -0300

    CRUD FINAL
    Co-authored-by: Pedro Henrique <pedrocorrea2002@users.noreply.github.com>

--- 20157027_Av2_Final.py
+++ 20157027_Av2_Final.py
@@ -1,13 +1,18 @@
 # David Renan
 # Pedro Corrêa
 
+# OBS instalar openpyxl, webdriver-manager e selenium
+
 # Importação de módulos
-import openpyxl
+import openpyxl as opx
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from datetime import date
+import csv
+
 
 # Seleciona o driver mais atualizado do webdriver e instala para previnir erros de atualização e versão do navegador.
 servico = Service(ChromeDriverManager().install())
@@ -15,6 +20,7 @@ navegador = webdriver.Chrome(service=servico)
 
 # Acessando site
 navegador.get('https://www.google.com/')
+
 
 #Acessando a busca
 navegador.find_element(By.NAME, 'q').send_keys('tempo agora Itatiaia-RJ')
@@ -27,28 +33,33 @@ navegador.find_element(By.NAME, 'btnK').click()
 
 # Encontrar temperatura
 texto = navegador.find_element(By.ID, 'wob_tm').text
+data = date.today()
 
-print ("A temperatura é: ", texto)
+
 
 # ------------------------------------------------------------------------------------------------
 
-# Criando planilha
-Plan = openpyxl.Workbook()
+#! VERIFICANDO SE O ARQUIVO EXISTE OU NÃO
 
-# Criando pagina especifico
-Plan.create_sheet('Temperaturas')
+plan = ""
+try : #EXISTE ===> EDITAR
+    plan = opx.load_workbook(filename = 'Planilha de temperaturas.xlsx')
 
-# Selecionando uma página
-pag_tempo = Plan['Temperaturas']
+    # Selecionando uma página
+    pag_tempo = plan['Temperaturas']
+except: #NÃO EXISTE ===> CRIAR
+    # Criando planilha
+    plan = opx.Workbook()
 
-# Inserindo registros
-pag_tempo.append(['Data', 'Temperatura'])
+# Renomeando aba
+    plan.active.title = "Temperaturas"   
+
+    # Selecionando uma página
+    pag_tempo = plan.active
+
+    # Inserindo registros
+    pag_tempo.append(['Data', 'Temperatura'])
+pag_tempo.append([data, texto])
 
 # Salvar planilha
-Plan.save ('Planilha de temperaturas.xlsx')
-time.sleep(5)
-
-# Carregando planilha
-Carreg_Plan = openpyxl.load_workbook('Planilha de temperaturas.xlsx')
-# selecionando aba da planilha
-Selec_tempo = Plan['Temperaturas']
+plan.save ('Planilha de temperaturas.xlsx')
